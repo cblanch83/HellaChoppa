ca918601b22672daabd565c6f3b3e260daa69af8
Estéfano Vignetta <Estfano Vignetta>
2016-07-23 16:49:10 -0300

    Merge de los dos trabajos.

--- Hellachoppa/Assets/Scripts/Player_Movement.cs
+++ Hellachoppa/Assets/Scripts/Player_Movement.cs
@@ -6,8 +6,9 @@ public class Player_Movement : MonoBehaviour {
     private Rigidbody rb;
     public float speed1, speed2;
     private float h1, v1, h2, v2;
-    public Transform turretRef1, turretRef2;
-    public ParticleSystem dust;
+	public Transform turretRef1, turretRef2, missileRef1, missileRef2;
+    public ParticleSystem dustPrefab;
+	public GameObject missilePrefab;
     public bool shooting;
     private RaycastHit rch;
 
@@ -30,8 +31,22 @@ public class Player_Movement : MonoBehaviour {
         v1 = Input.GetAxisRaw("LeftJoystickVertical");
         v2 = Input.GetAxisRaw("RightJoystickVertical");
 
-        shooting = Input.GetButton("AButton");
+        //shooting = Input.GetButton("AButton");
+		if (Input.GetButton("AButton"))
+			if (Physics.Raycast (turretRef1.transform.position, turretRef1.transform.forward, out rch))
+			{
+			ParticleSystem obj = (ParticleSystem)Instantiate(dustPrefab, rch.point, Quaternion.Euler(270, 0, 0));
+				//DestroyObject (obj, .1f);
+				Destroy(obj.gameObject,.1f);
+			}
+
+		if (Input.GetButtonDown ("BButton"))
+		{
+			GameObject obj = (GameObject)Instantiate (missilePrefab, missileRef1.transform.position + missileRef1.transform.forward, transform.rotation);
+			obj.GetComponent<Rigidbody> ().AddForce (missileRef1.transform.forward * 3000);
+		}
     }
+
     void FixedUpdate()
     {
         /*rb.AddRelativeForce(-Physics.gravity.y * rb.mass * Vector3.forward + Vector3.forward * v1 * speed1);
@@ -39,8 +54,6 @@ public class Player_Movement : MonoBehaviour {
         rb.AddRelativeForce(-Physics.gravity.y * rb.mass * Vector3.up + Vector3.up * v1 * speed1);
         rb.AddRelativeTorque(Vector3.right * v2 * speed2 - Vector3.forward * h1 * speed2 + Vector3.up * h2 * speed2);
 
-        if (shooting)
-            if (Physics.Raycast(turretRef1.transform.position, turretRef1.transform.forward, out rch))
-                Instantiate(dust, rch.point, Quaternion.Euler(270, 0, 0));
+
     }
 }
